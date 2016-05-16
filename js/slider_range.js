aa5ee363290fc899702787645c1541e3adc4188a
NOAM <elad.afif@gmail.com>
2016-05-16 19:58:04 +0300

    JS  for the first page slider

---
+++ js/slider_range.js
@@ -0,0 +1,25 @@
+/**
+ * Created by NOAM on 03/03/16.
+ */
+/*
+$(function() {
+    var el, newPoint, newPlace, offset;
+    $("input[type='range']").change(function() {
+        el = $(this);
+        width = el.width();
+        newPoint = (el.val() - el.attr("min")) / (el.attr("max") - el.attr("min"));
+        offset = -1.3;
+        if (newPoint < 0) { newPlace = 0;  }
+        else if (newPoint > 1) { newPlace = width; }
+        else { newPlace = width * newPoint + offset; offset -= newPoint;}
+        el
+            .next("output")
+            .css({
+                left: newPlace,
+                marginLeft: offset + "%"
+            })
+            .text(el.val());
+    })
+        .trigger('change');
+});
+*/
