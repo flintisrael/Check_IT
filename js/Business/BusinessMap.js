bbf53c3702d3b792fb4f6414a21795ec6b60d572
israelf <flintisrael@gmail.com>
2016-05-22 20:31:33 +0300

    test

---
+++ js/Business/BusinessMap.js
@@ -0,0 +1,138 @@
+$(function()
+{
+    //alert('elad');
+
+    //var username = sessionStorage.getItem('clicked');
+    // alert( sessionStorage.getItem('clicked'));
+
+    var map;
+    var marker;
+    var item;
+    var markers = [];
+
+
+
+    function initialize()
+    {
+
+        $.get( "getbusinesses.php", function( item )
+        {
+            item = JSON.parse(item);
+
+            for(var i = 0 ; i < (item.length) ; i++){
+
+                addMarker(item[i]);
+
+            }
+
+        });
+
+        var myLatlng =  {lat : 31.8,lng: 35.17};
+
+
+        var mapProp = {
+            //center:new google.maps.LatLng(32.0878802,34.7955294),
+            center: myLatlng,
+            zoom:10,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+
+
+        map = new google.maps.Map(document.getElementById("businessmap"),mapProp);
+
+
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                // debugger;
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('.המיקום שלי');
+                map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            //alert('Browser doesnt support Geolocation');
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'שנה הגדרות אפשר מיקום במכשיר' :
+            'Error: Your browser doesn\'t support geolocation.');
+
+
+
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    function addMarker(item){
+
+        var lat = item['Lat'];
+        var lng = item['Lon'];
+
+        if(lat == null || lng == null || lat == 0 || lng == 0) return;
+        var myLatlng = new google.maps.LatLng(lat,lng);
+
+        var bId = item['Id'];
+
+        var description = 'תיאור';
+        var participantsSring = '';
+        var name = item['Name'];
+        var PhotoURL = item['PhotoURL'];
+        PhotoURL = '../image/businessProfile/'+PhotoURL;
+
+
+
+
+
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h4 id="firstHeading" class="firstHeading" style="color:#DF7401"><a href="businessPage.php?bid='+bId+'">'+name.toString()+'</a></h4>'+
+            '<div id="bodyContent">'+
+            '</br>' +
+
+            '<img src="'+PhotoURL+'"style="width: 150px;height:100px">'+
+            '<div><a href="businessStaticsPage.php?bid='+bId+'" >Feedbacks Stats</a> </div>' +
+
+
+            '</div>'+
+            participantsSring
+        '</div>'+
+        '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,maxWidth: 350
+        });
+
+        var  marker =  new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: name
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+
+        markers.push(marker);
+        marker.setMap(map);
+
+
+    }
+
+});
+
