11d9e838430210363ab5201f047cadefd4241dcb
israelf <flintisrael@gmail.com>
2016-05-28 21:36:31 +0300

    get all businesses function from db

---
+++ function/getallbusinesses.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+include ($_SERVER['DOCUMENT_ROOT']."/checkit/database/connect.php");
+
+$sql = "
+  SELECT B.Id as bid ,B.*,BT.*
+  FROM
+    Business B
+  INNER JOIN
+    BusinessType BT on BT.id = B.refType
+  WHERE
+    B.state = 1 AND BT.State = 1";
+$result = mysql_query($sql);
+$array =array();
+
+while($row = mysql_fetch_array($result)) {
+
+    array_push($array,$row);
+
+}
+echo json_encode($array);
+
+
+?>
